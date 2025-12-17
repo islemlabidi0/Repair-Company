18e7ee1b851ae1c095d96c4b434b44ab0d9da1bc
Islem Labidi <labidiislem971@gmail.com>
2025-12-17 22:58:25 +0100

    feat✨: create device

---
+++ src/device/device.controller.ts
@@ -0,0 +1,18 @@
+import { DeviceService } from './device.service';
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { CreateDeviceDto } from './Dto/create-device.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+
+//lezm yebda user connected bch ynjm
+@UseGuards(JwtAuthGuard)
+@Controller('devices')
+export class DeviceController {
+    constructor(private readonly DeviceService: DeviceService) {}
+
+    @Post()
+    //@body to ready json body ml request 
+    //createDeviceDto 5tr lezm lbody match the createDeviceDto
+    create(@Body() CreateDeviceDto: CreateDeviceDto){
+        return this.DeviceService.create(CreateDeviceDto);
+    }
+}
---
+++ src/device/device.service.ts
@@ -0,0 +1,29 @@
+import { CreateDeviceDto } from './Dto/create-device.dto';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Device } from "./device.entity";
+
+@Injectable()
+export class DeviceService {
+    constructor(
+        @InjectRepository(Device)
+        //nestJS ya3ml device repository w yconnectiha b device table fi mysql
+        private readonly deviceRepository: Repository<Device>,
+    ){}
+
+    async create(CreateDeviceDto: CreateDeviceDto): Promise<Device> {
+        //nchoufou ken l device déjà mawjouda fl database
+        const existing = await this.deviceRepository.findOne({
+            where: { serialNumber: CreateDeviceDto.serialNumber},
+        });
+    
+    //ken mawjouda t5arej exception
+    if(existing){
+        throw new BadRequestException('Device with this serial number already exists');
+    }
+    //sinon tzid device fl base
+    const device = this.deviceRepository.create(CreateDeviceDto);
+    return this.deviceRepository.save(device);
+    }
+}
