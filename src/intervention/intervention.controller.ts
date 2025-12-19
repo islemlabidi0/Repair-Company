d8ce2739cf6e353bd60ad09565c7f3884f8becec
Islem Labidi <labidiislem971@gmail.com>
2025-12-19 17:43:09 +0100

    chore: guard change

--- src/intervention/intervention.controller.ts
+++ src/intervention/intervention.controller.ts
@@ -1,10 +1,9 @@
 import { InterventionService } from './intervention.service';
 import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
-import { IsTechGuard } from "./isTech-guard";
 import { CreateInterventionDto } from './Dto/create-intervention.dto';
 //ken technicien ynjm y3ml create interventions w lezm ykoun connecté 
-@UseGuards(JwtAuthGuard, IsTechGuard)
+@UseGuards(JwtAuthGuard)
 
 @Controller('interventions')
 export class InterventionController{
